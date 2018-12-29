13ffe2d86fba79c7c299367b99cc28301affc55c
Oğuz <37263350+oguzhalit@users.noreply.github.com>
2018-12-29 17:16:12 +0300

    Add files via upload
    Pointerlar tipe bakmaksızın 32bit sistemlerde 4byte
                                                64bit sistemlerde 8byte

---
+++ b4kte.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	int i;cout<<"i ="<<sizeof(i)<<endl;
+	double d;cout<<"d ="<<sizeof(d)<<endl;
+	float f;cout<<"f ="<<sizeof(f)<<endl;
+	char c;cout<<"c ="<<sizeof(c)<<endl;
+	int *pi;cout<<"pi ="<<sizeof(pi)<<endl;
+	double *pd;cout<<"pd ="<<sizeof(pd)<<endl;
+	float *pf;cout<<"pf ="<<sizeof(pf)<<endl;
+	char *pc;cout<<"pc ="<<sizeof(pc)<<endl;
+	
+}
